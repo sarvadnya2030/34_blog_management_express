ded95f94a2c6a9711edc0feb54a02b3fb5ae6f88
Sarvadnya <shripad.bhatlawande77@gmail.com>
2026-05-03 21:23:53 +0530

    Fix: use process.env.PORT for flexible port binding

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(bodyParser.json());
